40745a86cac8765d50530f1114fadc60ee15582e
obed vargas luna <240145@utxicotepec.edu.mx>
2026-02-03 09:51:18 -0600

    Sistema de Bienes Raices
    import express from express;

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.get("/", (req, res) => {
+    console.log("Saludos desde la web");
+    res.json({
+        status: 200,
+        message: "Solicitud recibido"
+    })
+})
+
+app.get("/", (req, res) => {
+    console.log("El usuario desea acceder al sistema");
+    res.status(200).send(`<h1>Bienvenido al sistema</h1>
+ <form>
+    <input type="text" placeholder="Usuario" />
+    <input type="password" placeholder="Contraseña" />
+    <button type="submit">Iniciar Sesión</button>
+ </form>`
+    );
+})
+
+app.get("Saludo/:nombre", (req, res) => 
+    {
+        const nombre = req.params;
+    console.log(`El usuario: ${nombre}`);
+    res.status(200).send(`<p>Bienvenido <b>${nombre}</b></p>`
+    );
+});
+
+app.listen(PORT, () => {
+    console.log(`El servidor esta iniciando en el puerto ${PORT}`);
+});
