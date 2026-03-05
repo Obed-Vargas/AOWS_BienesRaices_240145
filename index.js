c8ed2e178bf4d5f6fa2da822bf8e5b8dc71ded6b
obed vargas luna <240145@utxicotepec.edu.mx>
2026-03-05 09:40:56 -0600

    Bienes Raices
    add: se actualizo el inicio del servidor para impedir aaques por CSRF pero aun no concluye la proteccion

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from "express";
-import session from "express-session";
 import passport from "./config/passport.js";
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import { connectDB } from "./config/db.js";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import csurf from "@dr.pogodin/csurf";
 
 const app = express();
 
@@ -13,7 +15,36 @@ app.set("views", "./views")
 
 //Definimos la carpeta publica
 app.use(express.static('public'))
+
+//habilitamos lectura de datos a traves de request
 app.use(express.urlencoded({ extended: true }))
+
+//activamos la opcion para maniular la cookie - almacenamiento en el cliente (navegador)
+app.use(cookieParser());
+app.use(express.json());
+
+//definimos el middleware
+app.use(session({
+    secret: process.env.SESSION_SECRET || "BienesRaices_240145_csrf_secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production"
+    }
+}));
+
+
+//Habiotamos el mecanismo para proeccion de CSRF
+app.use(csurf())
+
+//habilitar los tokens de csrf para cualquier formulario
+app.use((req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+})
+
 
 // ─── Sesiones (requerido para OAuth) ──────────────────────────────────────
 app.use(session({
@@ -33,7 +64,7 @@ app.use("/", usuarioRoutes)
 
 await connectDB();
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`El servidor esta iniciando en el puerto ${PORT}`)
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`El servidor esta iniciando en el puerto ${process.env.PORT || 3000}`)
 })
